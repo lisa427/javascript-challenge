5620d6cda6e1672cebf5642a65e4e32215683f59
lisa427 <lisa.mcgowan4@gmail.com>
2020-12-17 22:00:30 -0500

    works and code refactored

--- static/js/app.js
+++ static/js/app.js
@@ -1,54 +1,70 @@
 // from data.js
-var tableData = data;
+const tableData = data;
 
 // Get a reference to the table body
-var tbody = d3.select("tbody");
+const tbody = d3.select("tbody");
 
-// display filtered data in table
-tableData.forEach((item) => {
-    var row = tbody.append("tr");
-    Object.entries(item).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+// function to display all data in table
+const allData = function() {
+
+    // clear table
+    document.getElementById("table-body").innerHTML = null;
+
+    // display all data in a table
+    tableData.forEach((item) => {
+        let row = tbody.append("tr");
+        Object.entries(item).forEach(([key, value]) => {
+          let cell = row.append("td");
+          cell.text(value);
+        });
     });
-});
+};
 
-// select the filter button
-var button = d3.select("#filter-btn");
-
-// select the form
-var form = d3.select("#form");
-
-// create event handlers 
-button.on("click", showData);
-form.on("submit",showData);
-
-// function to run when form is submitted
-function showData() {
+// function to display filtered data
+const filterData = function() {
 
     // Prevent the page from refreshing
     d3.event.preventDefault();
     
     // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
+    let inputElement = d3.select("#datetime");
   
     // Get the value property of the input element
-    var inputValue = inputElement.property("value");
+    let inputValue = inputElement.property("value");
     
     // filter the data by the input
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    let filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
     
-    // ENTER SOME CODE TO CLEAR THE TABLE
+    // clear the table before displaying filtered data
     document.getElementById("table-body").innerHTML = null;
 
     // display filtered data in table
     filteredData.forEach((item) => {
-        var row = tbody.append("tr");
+        let row = tbody.append("tr");
         Object.entries(item).forEach(([key, value]) => {
-          var cell = row.append("td");
+          let cell = row.append("td");
           cell.text(value);
         });
     });
-
-
 };
+
+// call function to display all data in table
+allData();
+
+// select the filter button
+const filterButton = d3.select("#filter-btn");
+
+// select the form
+const form = d3.select("#form");
+
+// select the clear filter button
+const clearButton = d3.select("#clear-btn");
+
+// create event handlers 
+
+// calls function to display filtered data
+filterButton.on("click", filterData);
+form.on("submit",filterData);
+
+// calls function to display all data
+clearButton.on("click", allData);
